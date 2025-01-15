b19fe9c4fcb0588bb949d3a33366e08e81c8c096
akitxxx <a.kawasaki.dev@gmail.com>
2025-01-15 10:11:40 +0900

    fix: schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,20 @@
 import { sql } from 'drizzle-orm';
-import { index, integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import {
+  index,
+  integer,
+  numeric,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 // usersテーブル
 export const usersTable = pgTable('users', {
-  id: text('id').primaryKey(),
-  nickname: text('nickname').notNull(),
+  id: uuid('id').primaryKey().defaultRandom(),
+  nickname: varchar('nickname', { length: 255 }).notNull(),
+
   createdAt: timestamp('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: timestamp('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
   deletedAt: timestamp('deleted_at'),
@@ -16,17 +26,18 @@ export type InsertUser = typeof usersTable.$inferInsert;
 export const subscriptionsTable = pgTable(
   'subscriptions',
   {
-    id: text('id').primaryKey(),
-    userId: text('user_id')
+    id: uuid('id').primaryKey().defaultRandom(),
+    userId: uuid('user_id')
       .notNull()
       .references(() => usersTable.id),
-    name: text('name').notNull(),
-    price: integer('price').notNull(),
-    cycle: text('cycle').notNull(),
+    name: varchar('name', { length: 255 }).notNull(),
+    price: numeric('price', { precision: 10, scale: 2 }).notNull(),
+    cycle: varchar('cycle', { length: 255 }).notNull(),
     startedAt: timestamp('started_at').notNull(),
     nextPaymentAt: timestamp('next_payment_at').notNull(),
     description: text('description'),
-    status: text('status').notNull(),
+    status: varchar('status', { length: 255 }).notNull(),
+
     createdAt: timestamp('created_at')
       .notNull()
       .default(sql`CURRENT_TIMESTAMP`),
@@ -35,6 +46,7 @@ export const subscriptionsTable = pgTable(
       .default(sql`CURRENT_TIMESTAMP`),
     deletedAt: timestamp('deleted_at'),
   },
+  // index
   (table) => ({
     userIdIdx: index('subscriptions_user_id_idx').on(table.userId),
     nextPaymentAtIdx: index('subscriptions_next_payment_at_idx').on(
