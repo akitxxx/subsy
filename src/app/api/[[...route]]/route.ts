69d6bc7f7beb512e25bfae0255fa833fd67a8a1f
akitxxx <a.kawasaki.dev@gmail.com>
2025-01-16 07:33:06 +0900

    feat: add hono

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+
+const app = new Hono().basePath('/api');
+
+app.get('/', (c) => c.text('Hello World'));
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+
+export type AppType = typeof app;
