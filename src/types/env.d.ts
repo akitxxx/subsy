2930e5b35eb70bbfbc150f786ebafea7013b8574
akitxxx <a.kawasaki.dev@gmail.com>
2025-01-22 07:32:51 +0900

    fix

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,7 +5,6 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     NEXT_PUBLIC_APP_ENV: 'development' | 'staging' | 'production' | 'test';
     DATABASE_URL: string;
-    AUTH_SECRET: string;
     AUTH_GOOGLE_CLIENT_ID: string;
     AUTH_GOOGLE_CLIENT_SECRET: string;
     NEXT_PUBLIC_API_URL: string;
