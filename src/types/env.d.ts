d1f2188db6886cd2d7e19b6c61e2fc7d241082a5
akitxxx <a.kawasaki.dev@gmail.com>
2025-01-22 09:38:38 +0900

    chore

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,10 +4,15 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     NEXT_PUBLIC_APP_ENV: 'development' | 'staging' | 'production' | 'test';
+
     DATABASE_URL: string;
+
     AUTH_GOOGLE_CLIENT_ID: string;
     AUTH_GOOGLE_CLIENT_SECRET: string;
+
+    // ex. http://localhost:3000
     NEXT_PUBLIC_API_HOST: string;
+    // supabase
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
   }
