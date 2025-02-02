aab03e5a77977b55fea92bc740de43f6f88195d8
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-03 08:14:30 +0900

    chore

--- src/lib/hono/hono.ts
+++ src/lib/hono/hono.ts
@@ -1,6 +1,8 @@
 import type { AppType } from '@/app/api/[[...route]]/route';
 import { hc } from 'hono/client';
 
-export const honoClient = hc<AppType>('http://localhost:3000');
+export const honoClient = hc<AppType>(
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+);
 
 export type HonoClient = typeof honoClient;
