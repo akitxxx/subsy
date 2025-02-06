4a2c4dde5eb36395454560c1f6be3443ef2408db
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-06 22:01:12 +0900

    fix: api url

--- src/lib/hono/hono.ts
+++ src/lib/hono/hono.ts
@@ -2,7 +2,7 @@ import type { AppType } from '@/app/api/[[...route]]/route';
 import { hc } from 'hono/client';
 
 export const honoClient = hc<AppType>(
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+  process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3000',
 );
 
 export type HonoClient = typeof honoClient;
