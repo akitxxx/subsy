5c3409c9500d1bed875cc740e4a0ed426f900f29
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-09 08:48:21 +0900

    fix: set relation in schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,15 +1,5 @@
-import { sql } from 'drizzle-orm';
-import {
-  index,
-  numeric,
-  pgTable,
-  primaryKey,
-  text,
-  timestamp,
-  unique,
-  uuid,
-  varchar,
-} from 'drizzle-orm/pg-core';
+import { relations, sql } from 'drizzle-orm';
+import { index, numeric, pgTable, primaryKey, text, timestamp, unique, uuid, varchar } from 'drizzle-orm/pg-core';
 
 // usersテーブル
 export const usersTable = pgTable('users', {
@@ -20,6 +10,10 @@ export const usersTable = pgTable('users', {
   updatedAt: timestamp('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
   deletedAt: timestamp('deleted_at'),
 });
+export const usersRelations = relations(usersTable, ({ one, many }) => ({
+  userAuth: one(userAuthsTable, { fields: [usersTable.id], references: [userAuthsTable.userId] }),
+  subscriptions: many(subscriptionsTable),
+}));
 export type SelectUser = typeof usersTable.$inferSelect;
 export type InsertUser = typeof usersTable.$inferInsert;
 
@@ -37,22 +31,18 @@ export const userAuthsTable = pgTable(
     // providerのuser id
     providerId: varchar('provider_id', { length: 255 }).notNull(),
 
-    createdAt: timestamp('created_at')
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`),
-    updatedAt: timestamp('updated_at')
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: timestamp('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
   },
   (table) => ({
     pk: primaryKey({ columns: [table.userId, table.provider] }),
     providerIdIdx: index('user_auths_provider_id_idx').on(table.providerId),
-    uniqueProviderConstraint: unique('user_auths_provider_unique_idx').on(
-      table.provider,
-      table.providerId,
-    ),
+    uniqueProviderConstraint: unique('user_auths_provider_unique_idx').on(table.provider, table.providerId),
   }),
 );
+export const userAuthsRelations = relations(userAuthsTable, ({ one }) => ({
+  user: one(usersTable, { fields: [userAuthsTable.userId], references: [usersTable.id] }),
+}));
 export type SelectUserAuth = typeof userAuthsTable.$inferSelect;
 export type InsertUserAuth = typeof userAuthsTable.$inferInsert;
 
@@ -72,22 +62,19 @@ export const subscriptionsTable = pgTable(
     description: text('description'),
     status: varchar('status', { length: 255 }).notNull(),
 
-    createdAt: timestamp('created_at')
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`),
-    updatedAt: timestamp('updated_at')
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: timestamp('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
     deletedAt: timestamp('deleted_at'),
   },
   // index
   (table) => ({
     userIdIdx: index('subscriptions_user_id_idx').on(table.userId),
-    nextPaymentAtIdx: index('subscriptions_next_payment_at_idx').on(
-      table.nextPaymentAt,
-    ),
+    nextPaymentAtIdx: index('subscriptions_next_payment_at_idx').on(table.nextPaymentAt),
     statusIdx: index('subscriptions_status_idx').on(table.status),
   }),
 );
+export const subscriptionsRelations = relations(subscriptionsTable, ({ one }) => ({
+  user: one(usersTable, { fields: [subscriptionsTable.userId], references: [usersTable.id] }),
+}));
 export type SelectSubscription = typeof subscriptionsTable.$inferSelect;
 export type InsertSubscription = typeof subscriptionsTable.$inferInsert;
