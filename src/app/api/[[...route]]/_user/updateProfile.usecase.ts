12072dbd913a3caaf0924b1e58dc58252e70475c
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-09 08:52:59 +0900

    fix

--- src/app/api/[[...route]]/_user/updateProfile.usecase.ts
+++ src/app/api/[[...route]]/_user/updateProfile.usecase.ts
@@ -21,15 +21,19 @@ type Output = {
 const run =
   ({ db, authUser }: Inject) =>
   async ({ nickname }: Input): Promise<Output> => {
-    const user = await db.query.usersTable.findFirst({
-      where: and(eq(userAuthsTable.providerId, authUser.id), isNull(usersTable.deletedAt)),
-    });
+    const [user] = await db
+      .select()
+      .from(usersTable)
+      .innerJoin(userAuthsTable, eq(usersTable.id, userAuthsTable.userId))
+      .where(and(eq(userAuthsTable.providerId, authUser.id), isNull(usersTable.deletedAt)))
+      .limit(1);
+
     if (!user) throw new NotFoundError('ユーザーが見つかりません');
 
     const [updatedUser] = await db
       .update(usersTable)
       .set({ nickname, updatedAt: new Date() })
-      .where(and(eq(usersTable.id, user.id), isNull(usersTable.deletedAt)))
+      .where(and(eq(usersTable.id, user.users.id), isNull(usersTable.deletedAt)))
       .returning();
 
     return { user: updatedUser };
