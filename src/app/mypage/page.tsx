4c60c3c5198d122fdce55b6cab70005ed4934fe0
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-09 08:05:35 +0900

    chore

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -9,14 +9,12 @@ import { Suspense } from 'react';
 
 async function getUser(): Promise<CurrentUser> {
   const cookieStore = await cookies();
-  console.time('getUser');
   const response = await honoClient.api.users.me.$get(
     {},
     {
       headers: { cookie: cookieStore.toString() },
     },
   );
-  console.timeEnd('getUser');
   if (!response.ok) {
     const errorData = await response.json();
 
