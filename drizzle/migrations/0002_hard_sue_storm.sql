329530d6b089e94912a5b5fccf22cb6fa59f225d
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-15 08:09:56 +0900

    chore: remove migrate

--- drizzle/migrations/0002_hard_sue_storm.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "user_auths" DROP CONSTRAINT "user_auths_user_id_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "user_auths" ADD CONSTRAINT "user_auths_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE cascade;
--- drizzle/migrations/0004_lucky_franklin_richards.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX "subscriptions_next_payment_at_idx";--> statement-breakpoint
-ALTER TABLE "subscriptions" ADD COLUMN "expired_at" timestamp with time zone NOT NULL;--> statement-breakpoint
-CREATE INDEX "subscriptions_expired_at_idx" ON "subscriptions" USING btree ("expired_at");--> statement-breakpoint
-ALTER TABLE "subscriptions" DROP COLUMN "next_payment_at";
