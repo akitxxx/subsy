ac214b763e63b2340558974581c33e72608fc43f
akitxxx <a.kawasaki.dev@gmail.com>
2025-02-15 13:05:06 +0900

    fix

--- src/app/api/_shared/presentation/view-model/subscription/parseSubscriptionViewModel.ts
+++ src/app/api/_shared/presentation/view-model/subscription/parseSubscriptionViewModel.ts
@@ -1,16 +1,18 @@
 import type { SubscriptionEntity } from '@/app/api/_shared/domain/subscription/subscription.entity';
 import { Subscription } from '@/app/api/_shared/domain/subscription/subscription.logic';
+import type { SubscriptionViewModel } from '@/domain/subscription/subscription.viewModel';
 
-export const parseSubscriptionViewModel = (entity: SubscriptionEntity) => {
+export const parseSubscriptionViewModel = (entity: SubscriptionEntity): SubscriptionViewModel => {
+  const now = new Date();
   return {
     ...entity,
     status: Subscription.getStatus(entity),
-    isInUse: Subscription.getIsInUse(entity),
+    isInUse: Subscription.getIsInUse(entity)(now),
     isCancelled: Subscription.getIsCancelled(entity),
-    isExpired: Subscription.getIsExpired(entity),
+    isExpired: Subscription.getIsExpired(entity)(now),
   };
 };
 
-export const parseSubscriptionsViewModel = (entities: SubscriptionEntity[]) => {
+export const parseSubscriptionsViewModel = (entities: SubscriptionEntity[]): SubscriptionViewModel[] => {
   return entities.map(parseSubscriptionViewModel);
 };
