a5cd870acc11b13ed12089b888e714627a8bc314
akitxxx <a.kawasaki.dev@gmail.com>
2025-10-07 08:25:26 +0900

    chore

--- src/shared/enums/subscription/subscriptionStatus.enum.ts
+++ src/shared/enums/subscription/subscriptionStatus.enum.ts
@@ -3,4 +3,5 @@ export const SubscriptionStatusEnum = {
   Cancelled: 'Cancelled', // 自動更新OFFだがまだ利用中
   Expired: 'Expired', // 期限切れして利用終了
 } as const;
+
 export type SubscriptionStatusEnum = (typeof SubscriptionStatusEnum)[keyof typeof SubscriptionStatusEnum];
